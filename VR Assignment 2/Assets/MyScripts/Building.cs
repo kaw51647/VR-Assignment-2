49d4eb9fc39a2805fed7db042c0b484ba198f7e1
kaw51647 <kaw51647@uga.edu>
2019-02-21 12:19:10 -0500

    Add existing file

---
+++ VR Assignment 2/Assets/MyScripts/Building.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.rigidbody == null)
+        {
+            return;
+        }
+
+        Rigidbody rb = collision.rigidbody;
+        Missile m = rb.GetComponent<Missile>();
+
+        if(m == null)
+        {
+            Debug.Log("Hit by " + rb.name);
+            return;
+        }
+       
+        Debug.Log("Destroyed by Missile!");
+        Destroy(this.gameObject);
+    }
+}
