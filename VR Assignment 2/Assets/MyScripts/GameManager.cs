cbbc84465cb2f9e26f850c6c7c894b20012b0185
kaw51647 <kaw51647@uga.edu>
2019-02-21 21:05:31 -0500

    Add existing file

---
+++ VR Assignment 2/Assets/MyScripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float missileSpeed;
+    public float missileSpawnPeriod_sec;
+    public float missileSpawnRadius;
+    public Transform missileSpawnOrigin;
+    public Missile missilePrefab;
+    public Transform target;
+    public bool missileSpawnerActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(missileSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator missileSpawner()
+    {
+        while (true)
+        {
+            if (missileSpawnerActive)
+            {
+                //spawn new missile
+                Missile m = Instantiate<Missile>(missilePrefab);
+
+                //move it to a new location
+                m.transform.position = missileSpawnOrigin.position
+                + Random.Range(-1.0f, 1.0f) * missileSpawnRadius * missileSpawnOrigin.right
+                + Random.Range(-1.0f, 1.0f) * missileSpawnRadius * missileSpawnOrigin.forward;
+
+                Rigidbody rb = m.GetComponent<Rigidbody>();
+
+                //if target is not destroyed
+                if (target != null)
+                {
+                    rb.velocity = (target.position - m.transform.position).normalized * missileSpeed;
+                }
+                yield return new WaitForSeconds(missileSpawnPeriod_sec);
+            }
+            yield return null;  //wait for next frame
+        }
+    }
+
+
+}
--- VR Assignment 2/Assets/MyScripts/Missile.cs
+++ VR Assignment 2/Assets/MyScripts/Missile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    public float lifeTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,11 @@ public class Missile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lifeTime -= Time.deltaTime;
+        if(lifeTime < 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
